923bd55c253643b20890ae3010f7e50786272114
Marcus Rådell <marcus@radell.net>
2025-04-23 21:47:56 +0200

    add basic axum code

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
-fn main() {
-    println!("Server started on port 3000.");
+use axum::{Router, routing::get};
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new().route("/", get(|| async { "Hi!" }));
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    println!("Server starting on port 3000.");
+    axum::serve(listener, app).await.unwrap();
 }
