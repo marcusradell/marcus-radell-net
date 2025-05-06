acd118c4f6be9aef4c475c43c8ddca9b86c24400
Marcus Rådell <marcus@radell.net>
2025-05-06 13:37:38 +0200

    add maud for html templating

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,12 @@
-use axum::{
-    Router,
-    routing::{get, head},
-};
+use axum::{Router, routing::get};
 use marcus_radell_net::kits::status::Health;
+use maud::{Markup, html};
+
+async fn get_home_page() -> Markup {
+    html! {
+        h1 { "Marcus Rådell - Tech Leader | Senior Fullstack Software Developer"}
+    }
+}
 
 async fn get_status() -> String {
     let health = Health::new();
@@ -13,7 +17,7 @@ async fn get_status() -> String {
 async fn main() -> shuttle_axum::ShuttleAxum {
     let router = Router::new()
         .route("/status", get(get_status))
-        .route("/", head(|| async {}));
+        .route("/", get(get_home_page));
     tracing::info!("Starting server!");
     Ok(router.into())
 }
