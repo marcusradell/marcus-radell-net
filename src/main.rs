062355e1dd0dc8b46b457ddd9c6d7bf060a024f7
Marcus Rådell <marcus@radell.net>
2025-05-06 14:44:34 +0200

    add favicon

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use axum::{Router, routing::get};
 use marcus_radell_net::kits::health::Health;
 use maud::{Markup, html};
+use tower_http::services::ServeFile;
 
 async fn get_home_page() -> Markup {
     html! {
@@ -13,7 +14,8 @@ async fn main() -> shuttle_axum::ShuttleAxum {
     let health = Health::new();
     let router = Router::new()
         .route("/", get(get_home_page))
-        .nest("/health", health.get_router());
+        .nest("/health", health.get_router())
+        .nest_service("/favicon.ico", ServeFile::new("public/favicon.ico"));
     tracing::info!("Starting server!");
     Ok(router.into())
 }
