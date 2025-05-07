6667971d1adbe196d6919c98e8599a2c82df8ec9
Marcus Rådell <marcus@radell.net>
2025-05-07 11:53:21 +0200

    setup project with ordinary Axum again

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use axum::{Router, routing::get};
 use marcus_radell_net::kits::health::Health;
 use maud::{Markup, html};
+use std::net::SocketAddr;
 use tower_http::services::ServeFile;
 
 async fn get_home_page() -> Markup {
@@ -9,13 +10,21 @@ async fn get_home_page() -> Markup {
     }
 }
 
-#[shuttle_runtime::main]
-async fn main() -> shuttle_axum::ShuttleAxum {
+#[tokio::main]
+async fn main() {
+    tracing_subscriber::fmt()
+        .with_max_level(tracing::Level::INFO)
+        .init();
+
     let health = Health::new();
     let router = Router::new()
         .route("/", get(get_home_page))
         .nest("/health", health.get_router())
         .nest_service("/favicon.ico", ServeFile::new("public/favicon.ico"));
-    tracing::info!("Starting server!");
-    Ok(router.into())
+
+    let addr = SocketAddr::from(([0, 0, 0, 0], 8080));
+    tracing::info!("Starting server on {}", addr);
+
+    let listener = tokio::net::TcpListener::bind(addr).await.unwrap();
+    axum::serve(listener, router).await.unwrap();
 }
